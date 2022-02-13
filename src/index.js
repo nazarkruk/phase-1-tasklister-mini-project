2b9b503fcc27150b05eaef2de0db43f237ccf35a
Nazar Kruk <nazarkruk@icloud.com>
2022-02-12 22:23:28 -0500

    add and delete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    
+    buildToDo(e.target.new_task_description.value)
+    form.reset()
+     
+    
+  })
+
 });
+
+function buildToDo(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  li.innerText = `${todo} ` 
+  btn.textContent = 'x'
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+
+}
